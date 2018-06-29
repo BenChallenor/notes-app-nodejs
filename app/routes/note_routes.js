87e05d1bc105e767e5697611074b825f05d1a0f4
Ben <ben.challenor@btopenworld.com>
2018-06-29 13:37:29 +0100

    Add update note route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -30,6 +30,21 @@ module.exports = function(app, db) {
     });
   });
 
+  app.put("/notes/:id", function(req, res) {
+    const id = req.params.id;
+    const details = {"_id": new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, function(err, item) {
+      if (err) {
+        res.send({
+          'error': 'An error has occured'
+        });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.post('/notes', function(req, res) {
     // // note cteated here
     // console.log(req.body)
