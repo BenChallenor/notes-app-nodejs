9fdf01636e3b0614962521d2c8ea5a396f5ec860
Ben <ben.challenor@btopenworld.com>
2018-06-29 13:22:23 +0100

    Add get note route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,10 +1,36 @@
+var ObjectID = require("mongodb").ObjectID
+
 module.exports = function(app, db) {
 
+  app.get("/notes/:id", function(req, res) {
+    const id = req.params.id;
+    const details = {"_id": new ObjectID(id) };
+    db.collection('notes').findOne(details, function(err, item) {
+      if (err) {
+        res.send({
+          'error': 'An error has occured'
+        });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   app.post('/notes', function(req, res) {
-    // note cteated here
-    console.log(req.body)
-    res.send("Hello World")
-    // the callback
-  })
+    // // note cteated here
+    // console.log(req.body)
+    // res.send("Hello World")
+    // // the callback
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, function(err, result) {
+      if (err) {
+        res.send({
+          'error': 'An error has occured'
+        });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
 
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const MongoClient = require ("mongodb").MongoClient;
 const bodyParser = require ("body-parser");
-
+const db = require ("./config/db");
 const app = express();
 
 
@@ -9,8 +9,14 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-require("./app/routes")(app, {});
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+
+
+MongoClient.connect(db.url, function(err, database) {
+  if (err) return console.log(err)
+  require("./app/routes")(app, database);
+  app.listen(port, function() {
+    console.log("Listening on " + port);
+  })
 })
